3795b3fe78df818a9cc39e74018be22bfb5891f9
james tuyuc <jaimetuyuc7@gmail.com>
2022-01-13 17:27:35 -0600

    UI changes for Chat screen
    Work with the Chat screen added dummy data to simulate the chats.

--- views/Chats/Chat.js
+++ views/Chats/Chat.js
@@ -1,35 +1,50 @@
 import React from 'react';
-import { SafeAreaView, View, Text, StyleSheet, TouchableWithoutFeedback, Dimensions } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, TouchableWithoutFeedback, TouchableOpacity, Dimensions, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import Global from '../../styles/GStyles';
 
 const { height, width } = Dimensions.get('screen');
 const Chat = ({user}) => {
     //console.log(user, 'from Chat')
+
+    let contactInfo = '';
+    if( user.name === ''){
+        contactInfo = user.phone
+    }else {
+        contactInfo = user.name
+    }
     return (  
         <>
             <SafeAreaView style={ styles.mainC}>
-                <TouchableWithoutFeedback
+                <TouchableOpacity
                     onPress={ () => console.log('chat opened', user.name)}
                     onLongPress={ () => console.log('more options for', user.name)}
                 >
                     <View style={styles.secondC}>
                         <View style={styles.imgC}>
-                            <TouchableWithoutFeedback>
-                                <Text>Imagen?</Text>
+                            <TouchableWithoutFeedback
+                                onPress={ () => console.log('modal for image')}
+                            >
+                                <Image
+                                    style={{ height: 45, width: width * 0.11, borderRadius: 45}}
+                                    source={ require('../../helpers/imgs/user.jpg')}
+                                />
                             </TouchableWithoutFeedback>
                         </View>
 
                         <View style={styles.contentC}>
                             <View style={styles.infoC}>
-                                <Text>numero</Text>
-                                <Text>Fecha</Text>
+                                <Text style={{ fontSize: 17, color: Global.textName, fontWeight: 'bold'}}>{contactInfo.toUpperCase() }</Text>
+                                <Text style={{ color: Global.textTime}}>{user.lastSeen}</Text>
                             </View>
-                            <View>
-                                <Text>mensaje</Text>
+                            <View style={ styles.textC}>
+                                <Text style={{ marginRight: 3}}>{ user.saw ? <Icon name='check-double' size={10} color={Global.saw} /> : <Icon name='check-double' size={10} color={Global.delivered} /> }</Text>
+                                <Text style={{ color: Global.textMessage}}>{user.lastMessage}</Text>
                             </View>
                         </View>
                     </View>
 
-                </TouchableWithoutFeedback>
+                </TouchableOpacity>
             </SafeAreaView>
         </>
     );
@@ -37,25 +52,31 @@ const Chat = ({user}) => {
 
 const styles = StyleSheet.create({
     mainC: {
-        //backgroundColor: 'gray',
+        //backgroundColor: 'blue',
         paddingHorizontal: 10,
-        height: 45,
     },
     secondC: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
     imgC: {
-        backgroundColor: 'gray',
-        width: width * 0.12,
+        //backgroundColor: 'gray',
+        width: width * 0.11,
+        borderRadius: 45,
+        height: 45
     },
     contentC: {
-        backgroundColor: 'pink',
+        //backgroundColor: 'pink',
         width: width * 0.82
     },
     infoC: {
         flexDirection:'row',
         justifyContent: 'space-between',
+    },
+    textC: {
+        flexDirection: 'row',
+        alignItems: 'center',
     }
 })
 
--- views/Chats/Chats.js
+++ views/Chats/Chats.js
@@ -1,27 +1,34 @@
 import React from 'react';
 import { SafeAreaView, Text, View, FlatList, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Chat from './Chat';
 import users from '../../helpers/Data';
+import Global from '../../styles/GStyles';
 
 const Chats = () => {
     const renderItem = ({item}) => {
         return (
             <>
-                <Chat
-                    user={item}
-                />
+                <View style={{ height: 65, justifyContent: 'center', marginVertical: 2}}>
+                    <Chat
+                        user={item}
+                    />
+                </View>
             </>
         )
     }
     return (  
         <>
-            <SafeAreaView>
-                <View style={{ marginTop: 15}}>
+            <SafeAreaView style={{ backgroundColor: Global.darkBackground}}>
+                <View style={{ marginTop: 5, marginBottom: 10}}>
                     <FlatList
                         data={users}
                         keyExtractor={ data => data.id}
                         renderItem={renderItem}
                     />
+                </View>
+                <View style={{ position: 'absolute', backgroundColor: 'green', bottom: 25, right: 20, height: 50, width: 50, borderRadius: 25, alignItems: 'center', justifyContent: 'center' }}>
+                    <Icon name="chat" size={25} color="white" />
                 </View>
             </SafeAreaView>
         </>
